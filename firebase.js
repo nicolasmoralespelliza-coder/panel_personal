98226a85411a1fa08d8b96d5cc5f942d7583ea28
nicolasmoralespelliza-coder <nicolasmoralespelliza@gmail.com>
2026-02-23 09:54:01 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  appId: "TU_APP_ID"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+console.log("🔥 Firebase inicializado");
