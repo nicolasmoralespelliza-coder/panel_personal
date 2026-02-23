8927173ac129e2f7995f02513b88abc37569f313
nicolasmoralespelliza-coder <nicolasmoralespelliza@gmail.com>
2026-02-23 11:24:11 -0300

    Refactor tareas.js to use tareasRef for Firestore

--- js/tareas.js
+++ js/tareas.js
@@ -1,32 +1,32 @@
 import { auth, db } from "./firebase.js";
 import {
-  collection, addDoc, onSnapshot, deleteDoc, updateDoc, doc
+  collection,
+  addDoc,
+  onSnapshot,
+  deleteDoc,
+  updateDoc,
+  doc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 let uid;
+let tareasRef;
 
 auth.onAuthStateChanged(user => {
-  if (user) uid = user.uid;
+  if (user) {
+    uid = user.uid;
+    tareasRef = collection(db, "usuarios", uid, "tareas");
+
+    escucharTareas();
+  }
 });
 
-window.agregarTarea = async () => {
-  const texto = nuevaTarea.value;
-  if (!texto) return;
-
-  await addDoc(collection(db, "usuarios", uid, "tareas"), {
-    texto,
-    completada: false
-  });
-
-  nuevaTarea.value = "";
-};
-
-onSnapshot(
-  () => collection(db, "usuarios", uid, "tareas"),
-  snapshot => {
+function escucharTareas() {
+  onSnapshot(tareasRef, snapshot => {
     listaTareas.innerHTML = "";
+
     snapshot.forEach(d => {
       const li = document.createElement("li");
+
       li.innerHTML = `
         <span style="text-decoration:${d.data().completada ? "line-through" : "none"}">
           ${d.data().texto}
@@ -34,17 +34,30 @@ onSnapshot(
         <button onclick="toggleTarea('${d.id}', ${d.data().completada})">✔</button>
         <button onclick="borrarTarea('${d.id}')">🗑</button>
       `;
+
       listaTareas.appendChild(li);
     });
-  }
-);
+  });
+}
+
+window.agregarTarea = async () => {
+  if (!nuevaTarea.value) return;
+
+  await addDoc(tareasRef, {
+    texto: nuevaTarea.value,
+    completada: false,
+    fecha: new Date()
+  });
+
+  nuevaTarea.value = "";
+};
 
 window.toggleTarea = async (id, estado) => {
-  await updateDoc(doc(db, "usuarios", uid, "tareas", id), {
+  await updateDoc(doc(tareasRef, id), {
     completada: !estado
   });
 };
 
 window.borrarTarea = async (id) => {
-  await deleteDoc(doc(db, "usuarios", uid, "tareas", id));
+  await deleteDoc(doc(tareasRef, id));
 };
