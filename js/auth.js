b9437527ec4b3b5ba76991383c4af09745400f9d
nicolasmoralespelliza-coder <nicolasmoralespelliza@gmail.com>
2026-03-05 09:28:21 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,22 +1,64 @@
-
 import { auth } from "./firebase.js";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+/* FUNCION PARA MOSTRAR MENSAJE */
+function mostrarMensaje(texto, tipo="success"){
+
+  const toast = document.getElementById("toast");
+
+  toast.textContent = texto;
+
+  toast.className = "show " + tipo;
+
+  setTimeout(() => {
+    toast.className = toast.className.replace("show", "");
+  },3000);
+
+}
+
+
+/* LOGIN */
 window.login = function () {
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   signInWithEmailAndPassword(auth, email, password)
-    .then(() => alert("✅ Login correcto"))
-    .catch(err => alert("❌ " + err.message));
+
+    .then(() => {
+
+      mostrarMensaje("Login correcto", "success");
+
+    })
+
+    .catch(err => {
+
+      mostrarMensaje(err.message, "error");
+
+    });
+
 };
 
+
+/* REGISTRO */
 window.registro = function () {
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   createUserWithEmailAndPassword(auth, email, password)
-    .then(() => alert("✅ Usuario creado"))
-    .catch(err => alert("❌ " + err.message));
+
+    .then(() => {
+
+      mostrarMensaje("Usuario creado", "success");
+
+    })
+
+    .catch(err => {
+
+      mostrarMensaje(err.message, "error");
+
+    });
+
 };
